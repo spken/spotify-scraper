5972606cf82056085ce2e12157844fb97115ccfa
spken <varematias11@gmail.com>
2024-02-02 23:58:00 +0100

    Clean up a bit, still stuff to do regarding cleanup

--- app.py
+++ app.py
@@ -1,89 +1,119 @@
-from flask import Flask, jsonify, redirect, render_template, request, session
-from dotenv import load_dotenv
-from flask_cors import CORS
-
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-
 import os
 import uuid
 
+import spotipy
+from dotenv import load_dotenv
+from flask import Flask, jsonify, redirect, render_template, request, session
+from flask_cors import CORS
+from spotipy.oauth2 import SpotifyOAuth
+
 load_dotenv()
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder="templates")
 app.secret_key = str(uuid.uuid4())
 CORS(app)
 
-client_id = os.getenv('SPOTIPY_CLIENT_ID')
-client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
-redirect_uri = os.getenv('SPOTIPY_REDIRECT_URI')
+client_id = os.getenv("SPOTIPY_CLIENT_ID")
+client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
+redirect_uri = os.getenv("SPOTIPY_REDIRECT_URI")
 
-cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
-scope = 'user-library-read user-read-currently-playing'
-auth_manager = SpotifyOAuth(
+CACHE_HANDLER = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+SCOPE = "user-library-read user-read-currently-playing"
+AUTH_MANAGER = SpotifyOAuth(
     client_id=client_id,
     client_secret=client_secret,
     redirect_uri=redirect_uri,
-    scope=scope,
+    scope=SCOPE,
     show_dialog=True,
-    cache_handler=cache_handler
+    cache_handler=CACHE_HANDLER,
 )
 
 
-@app.route('/')
+@app.route("/")
 def start():
-    return render_template('index.html')
+    """
+    Renders index on application launch
+    :return: Rendered index
+    """
+    return render_template("index.html")
 
 
-@app.route('/auth')
+@app.route("/auth")
 def authorize():
+    """
+    Handles spotify authorization flow
+    :return: Authorization URL for spotify
+    """
     # get the authorization url
-    auth_url = auth_manager.get_authorize_url()
+    auth_url = AUTH_MANAGER.get_authorize_url()
 
     # return the authorization url as json
-    return jsonify({'auth_url': auth_url})
+    return jsonify({"auth_url": auth_url})
 
 
-@app.route('/callback')
+@app.route("/callback")
 def callback():
-    # callback endpoint to handle the authorization response
-    code = request.args.get('code')
+    """
+    Callback endpoint to handle the authorization response
+    :return: A redirect to success / error page
+    """
+    code = request.args.get("code")
     if code:
-        token_info = auth_manager.get_access_token(code)
-        cache_handler.save_token_to_cache(token_info=token_info)
+        token_info = AUTH_MANAGER.get_access_token(code)
+        CACHE_HANDLER.save_token_to_cache(token_info=token_info)
 
-        return redirect('/success')
+        return redirect("/success")
     else:
-        return render_template('pages/error.html')
-    
+        return render_template("pages/error.html")
 
-@app.route('/success')
+
+@app.route("/success")
 def success():
-    return render_template('pages/success.html')
+    """
+    Renders the success template
+    :return: Render of success page
+    """
+    return render_template("pages/success.html")
 
 
-@app.route('/current')
-def currentTrack():
+@app.route("/logout")
+def logout():
+    """
+    Clears user session
+    :return: Default endpoint on logout
+    """
+    session.clear()
+    return jsonify({"logout": "/"})
+
+
+@app.route("/current")
+def current_track():
+    """
+    Gets a users current track (if one is playing)
+    :return: Track information
+    """
     # check if token is valid
-    if not auth_manager.validate_token(cache_handler.get_cached_token()):
-        return redirect('/auth')
+    if not AUTH_MANAGER.validate_token(CACHE_HANDLER.get_cached_token()):
+        return redirect("/auth")
 
     # attempt to refresh token if expired
-    if auth_manager.is_token_expired(cache_handler.get_cached_token()):
+    if AUTH_MANAGER.is_token_expired(CACHE_HANDLER.get_cached_token()):
         try:
-            new_token_info = auth_manager.refresh_access_token(cache_handler.get_cached_token()['refresh_token'])
-            cache_handler.save_token(new_token_info)
+            new_token_info = AUTH_MANAGER.refresh_access_token(
+                CACHE_HANDLER.get_cached_token()["refresh_token"]
+            )
+            CACHE_HANDLER.save_token_to_cache(new_token_info)
         except Exception as e:
-            return jsonify({'error': f'Token refresh failed: {str(e)}'})
+            return jsonify({"error": f"Token refresh failed: {str(e)}"})
 
-    sp = spotipy.Spotify(auth_manager=auth_manager)
-    current_track = sp.current_user_playing_track()
-    
-    if current_track:
-        return jsonify({'current_track': current_track})
+    sp = spotipy.Spotify(auth_manager=AUTH_MANAGER)
+    track = sp.current_user_playing_track()
+
+    if track:
+        return jsonify({"current_track": track})
     else:
-        return jsonify({'message': 'No track is playing right now.'})
+        return jsonify({"message": "No track is playing right now."})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
--- static/scripts/manager.js
+++ static/scripts/manager.js
@@ -14,7 +14,22 @@ async function getCurrentTrack() {
   fetch("/current")
     .then((response) => response.json())
     .then((data) => {
-      document.getElementById("currentTrack").textContent = data.current_track.item.name;
+      document.getElementById("link").textContent = data.current_track.item.name;
+      document.getElementById("link").href = data.current_track.item.external_urls.spotify;
+      document.getElementById("image").src = data.current_track.item.album.images[0].url;
+      document.getElementById("image").height = 210;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+async function logout() {
+  fetch("/logout")
+    .then((response) => response.json())
+    .then((data) => {
+      // redirect the user to index page
+      window.location.href = data.logout;
     })
     .catch((error) => {
       console.error("Error:", error);
