b46163235370cd2e92134c021dfa8ad74b88bedc
spken <varematias11@gmail.com>
2024-02-10 22:38:27 +0100

    Create baseline for content display

--- static/scripts/manager.js
+++ static/scripts/manager.js
@@ -13,7 +13,7 @@ async function getTopArtists(timeRange) {
     const response = await fetch(`/top-artists/${timeRange}`);
     const data = await response.json();
 
-    // TODO: do something with the data
+    displayItems(data, "artists");
 
   } catch (error) {
     console.error("Error:", error);
@@ -25,10 +25,29 @@ async function getTopTracks(timeRange) {
     const response = await fetch(`/top-tracks/${timeRange}`);
     const data = await response.json();
 
-    // TODO: do something with the data
+    displayItems(data, "tracks");
 
   } catch (error) {
     console.error("Error:", error);
+  }
+}
+
+const appendElement = (parentId, elementType, content) => {
+  const element = document.createElement(elementType);
+  element.textContent = content;
+  document.getElementById(parentId).appendChild(element);
+}
+
+function displayItems(data, content) {
+  if (content === "artists") {
+    // TODO: artist data
+  }
+  else if (content === "tracks") {
+    // TODO: track data
+    data.tracks.items.forEach((track, index) => {
+      const trackName = track.name;
+      appendElement("content", "p", `${index + 1}. ${trackName}`);
+    });
   }
 }
 
@@ -46,12 +65,14 @@ document.addEventListener('DOMContentLoaded', function () {
   const timeRangeButtons = document.querySelectorAll('.btn-group button');
 
   timeRangeButtons.forEach(function(button) {
-      button.addEventListener('click', function() {
-          timeRangeButtons.forEach(function(btn) {
-              btn.classList.remove('active');
-          });
-          this.classList.add('active');
-          document.getElementById('selected-time-range').value = this.getAttribute('data-range');
+    button.addEventListener('click', function() {
+      timeRangeButtons.forEach(function(btn) {
+        btn.classList.remove('active');
       });
+        this.classList.add('active');
+        document.getElementById('selected-time-range').value = this.getAttribute('data-range');
+    });
   });
 });
+
+// TODO: action listener for onload and selection
